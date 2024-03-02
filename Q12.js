4593f9c729fc7c39cb661a86e6782ece3c37e260
Abubakar <160392189+Abubakar-shafaqat@users.noreply.github.com>
2024-03-02 23:48:43 +0500

    Add files via upload
    Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.

---
+++ Q12.js
@@ -0,0 +1,7 @@
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+var name1 = ['Hello Abubakar!', 'Hello Hamza!', 'Hello Arham!', 'Hello Abdullah!'];
+var messege = "I'm here and ready to help. What's on your mind??";
+console.log(name1[0], messege);
+console.log(name1[1], messege);
+console.log(name1[2], messege);
+console.log(name1[3], messege);
