79d4edb2ac8650a326dfcb3dd8bf2d83dc02f23d
Abubakar <160392189+Abubakar-shafaqat@users.noreply.github.com>
2024-03-02 21:08:56 +0500

    Add files via upload
    You should create four lines that look like this:
    
    console.log(5 + 3)
    
    Your output should simply be four lines with the number 8 appearing once on each line.

---
+++ Q8.js
@@ -0,0 +1,11 @@
+//You should create four lines that look like this:
+//console.log(5 + 3)
+//Your output should simply be four lines with the number 8 appearing once on each line.
+console.log('-'.repeat(75));
+console.log(5 + 3);
+console.log('-'.repeat(75));
+console.log(2 * 4);
+console.log('-'.repeat(75));
+console.log(30 - 22);
+console.log('-'.repeat(75));
+console.log(40 / 5);
